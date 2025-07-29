6463474a3b178f22c4fc03e9dd62ea2e84ff3349
Isaac_Santos <isaacdsantos@hotmail.com>
2025-07-29 19:45:26 -0300

    Atualizado arquivo callback_Cplusplus.cpp com exemplo de definicao de tipo para armazenar funcao

--- callback_Cplusplus.cpp
+++ callback_Cplusplus.cpp
@@ -11,16 +11,21 @@ void executar_callback(const std::function<void(int)>& callback)
 	callback(23);
 }
 
+//tipo definido para armazenar funcao
+using TipoCallback =std::function<void(int)>;
+
 int main()
 {
 
-	executar_callback(funcao_callback);
+	TipoCallback callback = funcao_callback;
 	
+	executar_callback(funcao_callback);
 
 	//usando funcao lambda
 	executar_callback([](int valor){	
 		std::cout<<"Lambda callback chamado com valor "<<valor<<std::endl;
 	});
+	
 
 	return 0;
 }
